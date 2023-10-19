c548f7e8ca670f9087ca3e197d627f1a22ec4a6a
Francisco Retamar <franretamar123@gmail.com>
2023-10-19 20:24:26 -0300

    funciones constructoras

--- .js/objetos.js
+++ .js/objetos.js
@@ -1,41 +1,53 @@
 const cerveza1 = {
     marca: "Heineken",
-    tamaño: "1 litro",
+    tamanio: "1 litro",
     formato: "Botella",
     precio: 1000
 }
 
 const cerveza2 = {
     marca: "Brahma",
-    tamaño: "1 litro",
+    tamanio: "1 litro",
     formato: "Botella",
     precio: 800
 }
 
 const cerveza3 = {
     marca: "Quilmes",
-    tamaño: "1 litro",
+    tamanio: "1 litro",
     formato: "Botella",
     precio: 900
 }
 
 const cerveza4 = {
     marca: "Heineken",
-    tamaño: "500 cc",
+    tamanio: "500 cc",
     formato: "Lata",
     precio: 600
 }
 
 const cerveza5 = {
     marca: "Brahma",
-    tamaño: "500 cc",
+    tamanio: "500 cc",
     formato: "Lata",
     precio: 450
 }
 
 const cerveza6 = {
     marca: "Quilmes",
-    tamaño: "500 cc",
+    tamanio: "500 cc",
     formato: "Lata",
     precio: 500
 }
+
+//Funcion constructora
+
+function Cerveza (marca, tamanio, formato, precio) {
+    this.marca = marca;
+    this.tamanio = tamanio;
+    this.formato = formato;
+    this.precio = precio;
+}
+
+const cerveza7 = new Cerveza("Stella Artois", "1 litro", "Botella", 1100)
+const cerveza8 = new Cerveza("Stella Artois", "500 cc", "Lata", 600)
