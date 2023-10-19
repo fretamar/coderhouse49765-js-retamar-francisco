86059b1536559de6f83c99ca3b0e0be138bc49c8
Francisco Retamar <franretamar123@gmail.com>
2023-10-19 19:36:58 -0300

    objetos cerveza

---
+++ .js/objetos.js
@@ -0,0 +1,41 @@
+const cerveza1 = {
+    marca: "Heineken",
+    tamaño: "1 litro",
+    formato: "Botella",
+    precio: 1000
+}
+
+const cerveza2 = {
+    marca: "Brahma",
+    tamaño: "1 litro",
+    formato: "Botella",
+    precio: 800
+}
+
+const cerveza3 = {
+    marca: "Quilmes",
+    tamaño: "1 litro",
+    formato: "Botella",
+    precio: 900
+}
+
+const cerveza4 = {
+    marca: "Heineken",
+    tamaño: "500 cc",
+    formato: "Lata",
+    precio: 600
+}
+
+const cerveza5 = {
+    marca: "Brahma",
+    tamaño: "500 cc",
+    formato: "Lata",
+    precio: 450
+}
+
+const cerveza6 = {
+    marca: "Quilmes",
+    tamaño: "500 cc",
+    formato: "Lata",
+    precio: 500
+}
