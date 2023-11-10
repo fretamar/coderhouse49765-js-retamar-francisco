55bca8f25979a65c85e2567692c4791d40da1d25
Francisco Retamar <franretamar123@gmail.com>
2023-11-09 21:35:08 -0300

    agregue producto y producto literal

--- js/index.js
+++ js/index.js
@@ -50,33 +50,69 @@ function usuarioEdad() {
     else {
         console.log("Tenes que ingresar un nombre válido")
     }
-}   
+}
 
 //funcion con parametro
 
 let promocionPack6 = 5
 let promocionPack3 = 2.75
 
-function precioPack3(valor){
+function precioPack3(valor) {
     if (valor > 0) {
         let precioPack3 = valor * promocionPack3
-            console.log("Precio del pack con promoción:", precioPack3)
+        console.log("Precio del pack con promoción:", precioPack3)
     }
-    else{
+    else {
         console.error("Debes ingresar el precio de un producto")
     }
-}   
+}
 
-function precioPack6(valor){
+function precioPack6(valor) {
     if (valor > 0) {
         let precioPack6 = valor * promocionPack6
-            console.log("Precio del pack con promoción:", precioPack6)
+        console.log("Precio del pack con promoción:", precioPack6)
     }
-    else{
+    else {
         console.error("Debes ingresar el precio de un producto")
     }
-}   
+}
 
 //Document Object Model
 
-const tarjetaProducto = document.getElementsByClassName("tarjeta-producto")
+const creadorBebida = document.querySelector("div.tarjeta-producto#tarjetaBebida")
+
+function crearProducto(bebida) {
+    return `<div class="tarjeta-producto" id="tarjetaBebida">
+                <img class="imagen-producto" src="${bebida.imagen}" alt="Producto">
+                <h2 class="titulo-producto">${bebida.marca}</h2>
+                <p class="precio-producto">${bebida.precio}</p>
+                <label for="cantidad">Cantidad:</label>
+                <select class="selector-cantidad" id="cantidad" name="cantidad">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <button class="boton-agregar" onclick="agregarAlCarrito()">Agregar al Carrito</button>
+            </div>`
+}
+
+function productoError() {
+    return `<div class="tarjeta-error">
+                <img src="" alt="Icono de error">
+                <h2>Error en la Generación del Producto</h2>
+                <p>Lo sentimos, ha ocurrido un problema al intentar generar el producto. Por favor, inténtalo de nuevo más tarde.</p>
+                <button onclick="crearProducto()">Reintentar</button>
+            </div>`
+}
+
+function cargarBebida() {
+    if (bebidas.length > 0){
+        creadorBebida.innerHTML = ""
+        bebidas.forEach((bebida) => creadorBebida.innerHTML += crearProducto(bebida))
+    } else {
+        creadorBebida.innerHTML = productoError();
+    }
+}
+cargarBebida()
